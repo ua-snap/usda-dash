f18abd3698bc169ea237ca1219ca98ed775dbab2
Alec Bennett <alec@alecbennett.com>
2019-04-05 23:21:36 -0800

    Work to add user geolocation into Dash map.  Also allows more inclusion of Javascript

--- application.py
+++ application.py
@@ -1,332 +1,312 @@
 '''
 Template for SNAP Dash apps.
 '''
-
+import os
+import json
 import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_dangerously_set_inner_html as ddsih
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from dash.dependencies import Input, Output, State
+import visdcc
 
-app = dash.Dash(__name__)
+external_scripts = ['https://code.jquery.com/jquery-3.3.1.min.js']
+
+app = dash.Dash(external_scripts = external_scripts)
+
 # AWS Elastic Beanstalk looks for application by default,
 # if this variable (application) isn't set you will get a WSGI error.
 application = app.server
 
-app.title = 'SNAP Dash Template'
+app.title = 'SNAP - USDA Garden Helper'
 
-button_primary = html.Button('Primary', className='button')
-button_warning = html.Button('Primary', className='button is-warning')
-button_loading = html.Button('Primary', className='button is-info is-loading')
+mapbox_access_token = os.environ['MAPBOX_ACCESS_TOKEN']
+path_prefix = os.environ['REQUESTS_PATHNAME_PREFIX']
+communities = gpd.read_file('MainCommunities_4326.json')
 
-buttons_field = html.Div(
-    className='field',
+header_section = html.Div(
+    className='header',
     children=[
-        html.Label('Buttons', className='label'),
-        html.Div(className='buttons control', children=[
-            button_primary,
-            button_loading,
-            button_warning,
-        ]),
-        dcc.Markdown('You can see more info on different button styles and classes [here](http://www.somewhere.com)', className='help')
+        html.Div(
+            className='container',
+            children=[
+                html.Div(
+                    className='section',
+                    children=[
+                        html.Div(
+                            className='columns',
+                            children=[
+                                html.Div(
+                                    className='header--logo',
+                                    children=[
+                                        html.A(
+                                            className='header--snap-link',
+                                            href='https://snap.uaf.edu',
+                                            rel='external',
+                                            target='_blank',
+                                            children=[
+                                                html.Img(src=path_prefix + 'assets/SNAP_acronym_color_square.svg')
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                html.Div(
+                                    className='header--titles',
+                                    children=[
+                                        html.H1(
+                                            'USDA',
+                                            className='title is-2'
+                                        ),
+                                        html.H2(
+                                            'Explore local growing conditions under a changing climate.',
+                                            className='subtitle is-4'
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
     ]
 )
 
-dropdown = dcc.Dropdown(
-    options=[
-        {'label': 'New York City', 'value': 'NYC'},
-        {'label': 'Montréal', 'value': 'MTL'},
-        {'label': 'San Francisco', 'value': 'SF'}
-    ],
-    value='MTL'
-)
-dropdown_field = html.Div(
-    className='field',
-    children=[
-        html.Label('Dropdown', className='label'),
-        html.Div(className='control', children=[dropdown])
-    ]
-)
-
-multi_dropdown = dcc.Dropdown(
-    options=[
-        {'label': 'New York City', 'value': 'NYC'},
-        {'label': 'Montréal', 'value': 'MTL'},
-        {'label': 'San Francisco', 'value': 'SF'}
-    ],
-    multi=True,
-    value="MTL"
-)
-
-multi_dropdown_field = html.Div(
-    className='field',
-    children=[
-        html.Label('Multi-dropdown', className='label'),
-        html.Div(className='control', children=[multi_dropdown])
-    ]
-)
-
-slider = dcc.Slider(
-    min=-5,
-    max=10,
-    step=0.5,
-    value=-3,
-)
-slider_field = html.Div(
-    className='field',
-    children=[
-        html.Label('Slider', className='label'),
-        html.Div(className='control', children=[slider])
-    ]
-)
-
-range_slider = dcc.RangeSlider(
-    count=1,
-    min=-5,
-    max=10,
-    step=0.5,
-    value=[-3, 7]
-)
-range_slider_field = html.Div(
-    className='field',
-    children=[
-        html.Label('Range Slider', className='label'),
-        html.Div(className='control', children=[range_slider])
-    ]
-)
-
-marked_slider = dcc.Slider(
-    min=2000,
-    max=2090,
-    step=10,
-    marks={
-        2000: '2000\u2019s',
-        2050: '2050\u2019s',
-        2090: '2090\u2019s'
+map_communities_trace = go.Scattermapbox(
+    lat=communities['geometry'].y,
+    lon=communities['geometry'].x,
+    mode='markers',
+    marker={
+        'size': 15,
+        'color': 'rgb(80,80,80)'
     },
-    value=2030,
-)
-marked_slider_field = html.Div(
-    className='field',
-    children=[
-        html.Label('Marked Slider', className='label'),
-        html.Div(className='control', children=[marked_slider]),
-        html.Br(),
-        html.P("""
-            It's possible that the range marker labels will leak outside of the margins of the column, but they're still centered nicely.  Reduce the width of the slider if needed to keep spacing consistent.
-            """, className='help')
-    ]
+    line={
+        'color': 'rgb(0, 0, 0)',
+        'width': 2
+    },
+    text=communities.LocationName,
+    hoverinfo='text'
 )
 
-checkboxes = dcc.Checklist(
-    labelClassName='checkbox',
-    className='control',
-    options=[
-        {'label': ' New York City ', 'value': 'NYC'},
-        {'label': ' Montréal ', 'value': 'MTL'},
-        {'label': ' San Francisco ', 'value': 'SF'}
-    ],
-    values=['MTL', 'SF']
-)
-checkboxes_field = html.Div(
-    className='field',
-    children=[
-        html.Label('Checklist', className='label'),
-        checkboxes
-    ]
+map_layout = go.Layout(
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        zoom=3,
+        center=dict(lat=68, lon=-120),
+        layers=[
+            dict(
+                sourcetype='geojson',
+                source=json.loads(open('./AK.geo.json', 'r').read()),
+                type='fill',
+                color='rgba(255,255,255,.1)'
+            )
+        ]
+    ),
+    showlegend=False,
+    margin=dict(l=0, r=0, t=0, b=0)
 )
 
-radios = dcc.RadioItems(
-    labelClassName='radio',
-    className='control',
-    options=[
-        {'label': ' New York City ', 'value': 'NYC'},
-        {'label': ' Montréal ', 'value': 'MTL'},
-        {'label': ' San Francisco ', 'value': 'SF'}
-    ],
-    value='MTL'
-)
-radios_field = html.Div(
-    className='field',
-    children=[
-        html.Label('RadioItems', className='label'),
-        radios
-    ]
-)
+map_figure = go.Figure({
+    'data': [map_communities_trace],
+    'layout': map_layout
+})
 
 form_elements_section = html.Div(
     className='section',
     children=[
-        html.H2('Common form elements', className='title is-2'),
-        html.H4('These examples can be copy/pasted where appropriate.', className='subtitle is-4'),
         html.Div(
             className='columns',
             children=[
                 html.Div(
                     className='column',
                     children=[
-                        buttons_field,
-                        html.Br(),
-                        dropdown_field,
-                        html.Br(),
-                        multi_dropdown_field
-                    ]
-                ),
-                html.Div(
-                    className='column',
-                    children=[
-                        slider_field,
-                        html.Br(),
-                        range_slider_field,
-                        html.Br(),
-                        marked_slider_field
-                    ]
-                ),
-                html.Div(
-                    className='column',
-                    children=[
-                        checkboxes_field,
-                        html.Br(),
-                        radios_field
+                        html.Button('Update Location', id='location-button', n_clicks=0, className='button is-large'),
+
                     ]
                 )
             ]
-        )
+        ),
+        dcc.Graph(
+            id='map',
+            figure=map_figure,
+            config={
+                'displayModeBar': False,
+                'scrollZoom': False
+            },
+            style={'width': '100%', 'height': 600}
+        ),
+        dcc.RadioItems(
+            labelClassName='radio is-radio',
+            options=[
+                {'label': ' Basic', 'value': 'basic'},
+                {'label': ' Light', 'value': 'light'},
+                {'label': ' Dark', 'value': 'dark'},
+                {'label': ' Satellite', 'value': 'satellite-streets'},
+            ],
+            id='baselayer',
+            value='basic'
+        ),
+        visdcc.Run_js(id = 'javascript')
     ]
 )
 
-navbar = html.Div(
-    className='navbar',
-    role='navigation',
-    children=[
-        html.Div(className='navbar-brand', children=[
-            html.A(
-                className='navbar-item',
-                href='#',
-                children=[
-                    html.Img(src='assets/SNAP_acronym_color.svg')
-                ]
-            )
-        ]),
-        html.Div(className='navbar-menu', children=[
-            html.Div(className='navbar-start', children=[
-                html.A('Home', className='navbar-item'),
-                html.A('About', className='navbar-item'),
-                html.P('Navbar text', className='navbar-item')
-            ]),
-            html.Div(className='navbar-end', children=[
-                html.Div(className='navbar-item', children=[
-                    html.Div(className='buttons', children=[
-                        html.A('Sign up', className='button is-primary'),
-                        html.A('Log in', className='button is-light')
-                    ])
-                ])
-            ])
-        ])
-    ]
-)
-
-typography_section = html.Div(
-    className='section',
-    children=[
-        html.H1('Typography', className='title is-1'),
-        html.H3('Titles, text, and basic typography.', className='subtitle is-3'),
-        html.Div(
-            className='columns',
-            children=[
-                html.Div(
-                    className='column',
-                    children=[
-                        html.H2(
-                            'Header (h2)',
-                            className='title is-2',
-                        ),
-                        html.H4(
-                            'Subtitle, two levels smaller (h4), is spaced more closely to title. 👍',
-                            className='subtitle is-4'
-                        ),
-                        html.Br(),
-                        html.H3(
-                            'Header (h3)',
-                            className='title is-3'
-                        ),
-                        html.H5(
-                            'Subtitle, again two levels smaller (h5).  Better spacing!',
-                            className='subtitle is-5'
-                        ),
-                        html.H3(
-                            'General Subheader (h3)',
-                            className='subtitle is-3',
-                        ),
-                        html.H4(
-                            'General Subheader (h4)',
-                            className='subtitle is-4',
-                        ),
-                        html.H5(
-                            'General Subheader (h5)',
-                            className='subtitle is-5'
-                        )
-                    ]
-                ),
-                html.Div(
-                    className='column',
-                    children=[
-                        dcc.Markdown(
-                            """
-*Paragraph text*, `is-size-4`.  **Boutique eclectic** Asia-Pacific efficient charming airport liveable the highest quality Ginza [Winkreative impeccable](https://www.winkreative.com/) hub Lufthansa.
-
-* Nordic hub remarkable
-* Boeing 787 bulletin
-* Washlet sophisticated
-
- Espresso cosy iconic charming Singapore craftsmanship. Porter airport Boeing 787 Washlet bespoke Nordic K-pop intricate Ginza. Singapore liveable sharp smart bespoke finest conversation Gaggenau Asia-Pacific.
-                            """,
-                            className='is-size-4 content'
-                        ),
-                        html.Br(),
-                        dcc.Markdown(
-                            """
-*Paragraph text*, `is-size-6`.  **Sharp craftsmanship** sleepy, bureaux intricate cosy [Lufthansa Scandinavian exquisite](https://liveandletsfly.boardingarea.com/2018/06/28/lufthansa-first-class-houston/).
-
-* Flat white perfect,
-* Culpa handsome tote bag,
-* Occaecat Scandinavian Shinkansen,
-
- Espresso sharp iconic consectetur wardrobe, charming delightful ut eiusmod Comme des Garçons nisi conversation exercitation laboris Muji. Intricate finest dolor, Baggu liveable dolore id Melbourne Fast Lane Singapore. Lufthansa ut Shinkansen liveable.
-""",
-                            className='is-size-6 content'
-                        )
-                    ]
-                )
-            ]
-        )
-    ]
-)
 
 footer = html.Footer(
-    className='footer',
+    className='footer has-text-centered',
     children=[
         html.Div(
-            className='content has-text-centered',
             children=[
-                dcc.Markdown("""
-This is a page footer, where we'd put legal notes and other items.
-                    """)
+                html.A(
+                    href='https://snap.uaf.edu',
+                    target='_blank',
+                    className='level-item',
+                    children=[
+                        html.Img(
+                            src=path_prefix + 'assets/SNAP.svg'
+                        )
+                    ]
+                ),
+                html.A(
+                    href='https://uaf.edu/uaf/',
+                    target='_blank',
+                    className='level-item',
+                    children=[
+                        html.Img(
+                            src=path_prefix + 'assets/UAF.svg'
+                        )
+                    ]
+                )
             ]
+        ),
+        dcc.Markdown(
+            """
+            Test tool for USDA
+            """,
+            className='content is-size-6'
         )
     ]
 )
 
 app.layout = html.Div(
-    className='container',
     children=[
-        navbar,
-        typography_section,
-        html.Hr(),
-        form_elements_section,
-        footer
+        header_section,
+        html.Div(
+            className='section',
+            children=[
+                html.Div(
+                    className='container',
+                    children=[
+                        html.Div(id='location', className='container', style={ 'visibility': 'hidden' }),
+                        form_elements_section,
+                        footer
+                    ]
+                )
+            ]
+        )
     ]
 )
+
+@app.callback(
+    Output('javascript', 'run'),
+    [Input('location-button', 'n_clicks')])
+def myfun(x): 
+    if x is None: return ''
+    return '''
+      getPosition()
+      .then((position) => {
+          var x = position.coords.latitude + ',' + position.coords.longitude;
+          function updateLoc(position){
+                setProps({ 
+                'event': {
+                        'x': position.coords.latitude, 
+                        'y': position.coords.longitude
+                        }
+                });
+        }
+          $('#location').html(x);
+          var target = $('#location-button')[0];
+          $('#location-button').off();
+          target.addEventListener('click', updateLoc(position));
+      });
+        '''
+
+@app.callback(
+    Output('map', 'figure'),
+    [Input('javascript', 'event'), Input('baselayer', 'value')]
+)
+def update_map(user_loc, baselayer):
+    user_trace = {}
+    lats = [-65]
+    lons = [-147]
+    lzoom = 5
+    if user_loc is not None:
+        lats = [user_loc['x']]
+        lons = [user_loc['y']]
+        lzoom = 13
+        user_trace = go.Scattermapbox(
+            lat= lats,
+            lon= lons,
+            mode='markers',
+            marker={
+                'size': 20,
+                'color': 'rgb(30,70,250)',
+                'opacity': 0.7
+            },
+            text='User',
+            hoverinfo='text'
+        )
+    map_communities_trace = [
+        go.Scattermapbox(
+            lat=communities['geometry'].y,
+            lon=communities['geometry'].x,
+            mode='markers',
+            marker={
+                'size': 12,
+                'color': 'rgb(80,80,80)'
+            },
+            line={
+                'color': 'rgb(0, 0, 0)',
+                'width': 2
+            },
+            text=communities.LocationName,
+            hoverinfo='text'
+        ),
+        user_trace
+    ]
+
+    map_layout = go.Layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=dict(
+            accesstoken=mapbox_access_token,
+            zoom=lzoom,
+            center=dict(lat=lats[0], lon=lons[0]),
+            layers=[
+                dict(
+                    sourcetype='geojson',
+                    source=json.loads(open('./AK.geo.json', 'r').read()),
+                    type='fill',
+                    color='rgba(255,255,255,.1)'
+                )
+            ],
+            style=baselayer
+        ),
+        showlegend=False,
+        margin=dict(l=0, r=0, t=0, b=0)
+        )
+
+    map_figure = go.Figure({
+        'data': map_communities_trace,
+        'layout': map_layout
+    })
+
+    return map_figure
 
 if __name__ == '__main__':
     application.run(debug=True, port=8080)
