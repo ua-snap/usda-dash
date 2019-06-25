3d5b28c7871e2fbf0563977201cd97d62821d6d3
Alec Bennett <alec@alecbennett.com>
2019-06-25 14:54:31 -0800

    Restore REQUESTS env

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ external_scripts = ['https://code.jquery.com/jquery-3.3.1.min.js']
 
 server = flask.Flask(__name__)
 app = dash.Dash(external_scripts = external_scripts, server=server)
-#app.config.requests_pathname_prefix = os.environ['REQUESTS_PATHNAME_PREFIX']
+app.config.requests_pathname_prefix = os.environ['REQUESTS_PATHNAME_PREFIX']
 
 # AWS Elastic Beanstalk looks for application by default,
 # if this variable (application) isn't set you will get a WSGI error.
